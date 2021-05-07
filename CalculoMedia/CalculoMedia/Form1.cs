88db7419940c2d0cd1a9dc71473dadaa288037a7
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2021-05-06 22:14:08 -0300

    Calculo da Média(projeto em andamento)

---
+++ CalculoMedia/CalculoMedia/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculoMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float n1, n2, resultado;
+
+        }
+    }
+}
