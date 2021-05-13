7aca47a5c4a0e4c2666a53e65c3cd22e0ca150f0
Vinicius Rocha <37674676+ViniciusRock@users.noreply.github.com>
2021-05-13 16:54:40 -0300

    Condicao_if_else

---
+++ Condicao_if/Condicao_if/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Condicao_if
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.btnverifica = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(80, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a  sua idade";
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Location = new System.Drawing.Point(80, 89);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(161, 23);
+            this.txtIdade.TabIndex = 1;
+            // 
+            // btnverifica
+            // 
+            this.btnverifica.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnverifica.Location = new System.Drawing.Point(111, 127);
+            this.btnverifica.Name = "btnverifica";
+            this.btnverifica.Size = new System.Drawing.Size(103, 50);
+            this.btnverifica.TabIndex = 2;
+            this.btnverifica.Text = "Verificar idade";
+            this.btnverifica.UseVisualStyleBackColor = true;
+            this.btnverifica.Click += new System.EventHandler(this.txtverifica_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 236);
+            this.Controls.Add(this.btnverifica);
+            this.Controls.Add(this.txtIdade);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.Button btnverifica;
+    }
+}
+
---
+++ Condicao_if/Condicao_if/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Condicao_if
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtverifica_Click(object sender, EventArgs e)
+        {
+            int idade;
+            idade = Convert.ToInt32(txtIdade.Text);
+            if (idade >= 18)
+            {
+                MessageBox.Show("Maior de Idade", "Idade verificada!", MessageBoxButtons.OK, MessageBoxIcon.None);
+            }
+            else
+            {
+                MessageBox.Show("Menor de Idade", "Idade verificada!", MessageBoxButtons.OK, MessageBoxIcon.None);
+
+            }
+            
+        }
+    }
+}
